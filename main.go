1ac59b3479eb239797596767cc28238b94749b15
daoda97 <daodao97@foxmail.com>
2020-08-26 10:31:27 +0800

    perf: recover pkg

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 // app.json 存放运行时配置, 如 db/redis 配置, 由 pkg/config 持有
 // pkg 不能依赖 config/**.go 否则很容易出现循环依赖
 func main() {
-	boot := pkg.Bootstrap{
+	boot := egin.Bootstrap{
 		HttpMiddlewares: config.HttpMiddlewares,
 		RoutesMap:       config.Routes,
 		RoutesGroup:     config.RoutesGroup,
